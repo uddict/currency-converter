57c9db8b8aa6378277956de0718993f95f129acd
Uddict <uditkp024@gmail.com>
2024-10-08 09:44:21 +0530

    App complition

--- src/App.js
+++ src/App.js
@@ -1,23 +1,99 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import CurrencyInput from "react-currency-input-field";
+import "./styles.css";
 
 function App() {
+  const [rates, setRates] = useState();
+  const [ratesFetched, setRatesFetched] = useState(false);
+  const [amount, setAmount] = useState(0);
+  const [fromCurrency, setFromCurrency] = useState("USD");
+  const [toCurrency, setToCurrency] = useState("INR");
+  const [output, setOutput] = useState();
+
+  const getRates = async () => {
+    // fetch the data from API
+    const response = await fetch(
+      "https://v6.exchangerate-api.com/v6/59d560835c4e8d4f996adc83/latest/USD"
+    ).then((response) => response.json());
+
+    // save the rates in the state
+    if (response.result === "success") {
+      setRates(response.conversion_rates);
+      setRatesFetched(true);
+    }
+  };
+
+  useEffect(() => {
+    getRates();
+  }, []);
+
+  const calculateOutput = async () => {
+    // fetch the selected from currency rates
+    const response = await fetch(
+      `https://v6.exchangerate-api.com/v6/59d560835c4e8d4f996adc83/latest/${fromCurrency}`
+    ).then((response) => response.json());
+    const fetchedRates = response.conversion_rates;
+    const CurrencyRate = fetchedRates[toCurrency];
+    const output = amount * CurrencyRate;
+    setOutput(output);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <div className="container">
+      <div className="input-amount">
+        <label>Amount:</label>
+        <CurrencyInput
+          value={amount}
+          onValueChange={(amount) => setAmount(amount)}
+          intlConfig={{ locale: "en-US", currency: fromCurrency }}
+          allowDecimals={true}
+          allowNegativeValue={false}
+        />
+      </div>
+
+      <div className="input-from">
+        <label>From:</label>
+        <select
+          id="from"
+          value={fromCurrency}
+          onChange={(e) => setFromCurrency(e.target.value)}
         >
-          Learn React
-        </a>
-      </header>
+          {ratesFetched ? (
+            Object.keys(rates).map((currency, index) => (
+              <option key={index} value={currency}>
+                {currency}
+              </option>
+            ))
+          ) : (
+            <option defaultValue>USD</option>
+          )}
+        </select>
+      </div>
+
+      <div className="input-to">
+        <label>To:</label>
+        <select
+          id="to"
+          value={toCurrency}
+          onChange={(e) => setToCurrency(e.target.value)}
+        >
+          {ratesFetched ? (
+            Object.keys(rates).map((currency, index) => (
+              <option key={index} value={currency}>
+                {currency}
+              </option>
+            ))
+          ) : (
+            <option defaultValue>INR</option>
+          )}
+        </select>
+      </div>
+      <button className="btn" onClick={() => calculateOutput()}>
+        Calculate
+      </button>
+      <div className="output">
+        <label>Output: {output}</label>
+      </div>
     </div>
   );
 }
